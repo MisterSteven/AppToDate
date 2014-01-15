0346d37619b3f1e321fc52d0e190c4d566df3260
Lasse Häll <domestus@hotmail.com>
2014-01-15 15:07:23 +0100

    CalendarView DateChangeListener added

--- AppToDate/src/com/apptodate/MainActivity.java
+++ AppToDate/src/com/apptodate/MainActivity.java
@@ -6,13 +6,33 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	CalendarView cal;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		cal = (CalendarView) findViewById(R.id.calendarView1);
+		
+		cal.setOnDateChangeListener(new OnDateChangeListener() {
+			
+			@Override
+			public void onSelectedDayChange(CalendarView view, int year, int month,
+					int dayOfMonth) {
+				// TODO Auto-generated method stub
+				
+				Toast.makeText(getBaseContext(),"Selected Date is\n\n"
+					+dayOfMonth+" : "+month+" : "+year , 
+					Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	@Override
